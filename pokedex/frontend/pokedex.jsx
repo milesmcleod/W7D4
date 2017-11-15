453f2a07c6b2303e6c954a981d4a0c48a82ba0d6
miles mcleod <milesmcleod123@gmail.com>
2017-11-15 14:42:16 -0800

    Complete reducer and store functionality

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", ()=> {
+
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.fetchAllPokemon = fetchAllPokemon;
+  const store = configureStore();
+  window.store = store;
   const content = document.getElementById('content');
   ReactDOM.render(<div>Yo</div>, content);
 });
---
+++ pokedex/frontend/reducers/pokemon_reducer.js
@@ -0,0 +1,18 @@
+import { RECEIVE_ALL_POKEMON } from '../actions/pokemon_actions';
+import { merge } from 'lodash';
+
+
+const pokemonReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let newState;
+  switch(action.type){
+    case RECEIVE_ALL_POKEMON:
+      newState = merge({}, state, action.pokemon);
+      return newState;
+    default:
+      return state;
+  }
+};
+
+
+export default pokemonReducer;
